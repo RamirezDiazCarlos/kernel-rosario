4471b9dfca9a7ef270fd2e78c34456d57201518e
Charly <cramirezdiaz.dev@gmail.com>
2026-02-20 09:11:28 -0300

    ajustes para indexación en google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://kernel-rosario.vercel.app',
   output: 'static',
+  integrations: [sitemap()],
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
